5b2e24e4554b38324e876fd42e5cbacd7051ad7d
Staremang <Staremang@ya.ru>
2017-06-29 20:25:43 +0700

    Обновление

--- js-source/main.js
+++ js-source/main.js
@@ -5,11 +5,33 @@ function ready(fn) {
 		document.addEventListener('DOMContentLoaded', fn);
 	}
 }
-function setPlatform () {
+ready(function () {
 	if (navigator.platform.indexOf('Mac') !== -1 || navigator.platform.indexOf('iPhone') !== -1) {
 		document.querySelector('body').classList.add('ios')
 	} else {
 		document.querySelector('body').classList.add('android')
 	}
-}
-ready(setPlatform ());
+});
+$(document).ready(function() {
+	if (document.documentElement.clientWidth <= 768) {
+		$('.carousel').addClass('owl-carousel owl-theme');
+		$('.carousel').owlCarousel({
+			loop:true,
+			nav:true,
+			margin: 7,
+			navText: [ '', '' ],
+			responsive : {
+				0 : {
+					items:2
+				},
+				440 : {
+					items:3
+				},
+				560 : {
+					items:4
+				}
+			}
+		})
+	}
+})
+
