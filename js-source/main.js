767b9fefddc4d7997767f8709e6d7d9843aa3fd2
Staremang <Staremang@ya.ru>
2017-07-20 02:18:28 +0700

    Update #5

--- js-source/main.js
+++ js-source/main.js
@@ -39,14 +39,13 @@ Element.prototype.viewChecker = function (options) {
 	// };
 
 	var trigger = false,
-		canPlay = false,
+		isLoad = false,
 		played = false,
-		el = this,
-		video = el.children[0];
+		el = this;
+		// video = el.children[0];
 
-	if (document.getElementsByTagName('video')[0].canPlayType("video/mp4") !== '') {
-
-		video.setAttribute('preload', 'none');
+	if (isCanPlay()) {
+		var video = document.createElement('video');
 
 		if (navigator.platform.indexOf('Mac') !== -1 || navigator.platform.indexOf('iPhone') !== -1) {
 			video.setAttribute('src', 'video_ios/' + options);
@@ -54,45 +53,78 @@ Element.prototype.viewChecker = function (options) {
 			video.setAttribute('src', 'video_android/' + options);
 		}
 
-		video.load();
+		// video.setAttribute('preload', 'none');
 
-		video.addEventListener('canplaythrough', function () {
-			canPlay = true;
-			visibleTrigger();
-		})
-		attachEvent(video, 'error', end);
-		attachEvent(window, "scroll", visibleTrigger);
-		attachEvent(window, "resize", visibleTrigger);
+		video.classList.add('mediaobject-element');
+
+		el.insertBefore(video, el.children[0]);
+
+
+
+		video.addEventListener('canplaythrough', videoLoaded)
+		video.addEventListener('error', end);
+		window.addEventListener('scroll', visibleTrigger);
+		window.addEventListener('resize', visibleTrigger);
 		visibleTrigger();
+
+		function videoLoaded () {
+			isLoad = true;
+			visibleTrigger();
+		}
+
+		function visibleTrigger () {
+			if (isVisible() && !played) {
+
+				if (isLoad) {
+					played = true;
+					start();
+				} else {
+					setTimeout(function () {
+						if (!isLoad) {
+							video.removeEventListener('canplaythrough', videoLoaded)
+							video.removeEventListener('error', end);
+							window.removeEventListener('scroll', visibleTrigger);
+							window.removeEventListener('resize', visibleTrigger);
+							end();
+						}
+					}, 8000)
+				}
+			}
+		};
+
+		function start () {
+			const promise = video.play();
+
+			video.style.opacity = '1';
+			video.style.visibility = 'visible';
+
+			if (promise instanceof Promise) {
+				promise.then(function() {
+					console.log(options, 'autoplay')
+				}).catch(function(error) {
+					console.error(options, error.message);
+					end();
+				});
+
+			} else {
+				console.error('autoplay unknown');
+				end();
+			}
+
+			window.removeEventListener('scroll', visibleTrigger);
+			window.removeEventListener('resize', visibleTrigger);
+			video.addEventListener('ended', end)
+		};
+
 	} else {
 		end();
 	}
 
-	function visibleTrigger () {
-		if (isVisible() && !played) {
-
-			if (canPlay) {
-				played = true;
-				start();
-				console.log('play');
-			} else {
-				setTimeout(function () {
-					if (!canPlay) {
-						end();
-					}
-				}, 8000)
-			}
-		}
-	};
-
-	function start () {
-		video.play();
-		video.classList.add('animated');
-		video.classList.add('fadeIn');
-		video.addEventListener('ended', end)
-	};
 	function end () {
-		el.classList.add('ended');
+		el.classList.add('media-end');
+	}
+	function isCanPlay () {
+		return document.getElementsByTagName('video')[0].canPlayType("video/mp4") !== ''
 	}
 	function isVisible () {
 		return el.getBoundingClientRect().top <= document.documentElement.clientHeight;
@@ -112,15 +144,17 @@ Element.prototype.viewChecker = function (options) {
 
 ready(function () {
 	if (navigator.platform.indexOf('Mac') !== -1 || navigator.platform.indexOf('iPhone') !== -1) {
-		document.body.classList.add('ios')
+		document.body.classList.add('ios');
+		document.querySelector('.header-switch__item[data-os="ios"]').classList.add('active');
 	} else {
-		document.body.classList.add('android')
+		document.body.classList.add('android');
+		document.querySelector('.header-switch__item[data-os="android"]').classList.add('active');
 	}
 	document.querySelector('.section-hero__video-container').viewChecker('1.mp4');
 	document.querySelector('.three-phones-container').viewChecker('2.mp4');
-	// document.querySelector('.account-state-screenshot-video').viewChecker('3.mp4');
+		// document.querySelector('.account-state-screenshot-video').viewChecker('3.mp4');
 	document.querySelector('.three-phones-at-an-angle-video').viewChecker('4.mp4');
-	// document.querySelector('.screenshot-of-the-current-price-video').viewChecker('5.mp4');
+		// document.querySelector('.screenshot-of-the-current-price-video').viewChecker('5.mp4');
 	document.querySelector('.horizontal-phone-video').viewChecker('6.mp4');
 
 //	fadeIn(document.querySelector('.section-hero__video'));
@@ -130,6 +164,17 @@ ready(function () {
 	// document.querySelector('.section-hero__video').classList.add('fadeIn');
 });
 $(document).ready(function() {
+	$('.header-switch__item').click(function () {
+		$('.header-switch__item').removeClass('active');
+		$(this).addClass('active');
+		if ($(this).attr('data-os') === 'ios') {
+			$('body').removeClass('android');
+			$('body').addClass('ios');
+		} else {
+			$('body').removeClass('ios');
+			$('body').addClass('android');
+		}
+	})
 	if (document.documentElement.clientWidth <= 768) {
 		$('.carousel').addClass('owl-carousel owl-theme');
 		$('.carousel').owlCarousel({
